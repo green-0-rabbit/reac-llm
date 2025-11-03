3ca485f39d716c2a31d6d9b27d3a918fc2296b19
Kenneth Ag <kenneth.ag@humaapi.com>
2025-11-03 22:19:28 +0100

    Add initial Terraform configuration files for multi-environment setup

---
+++ .cloud/tf/prod.tfvars
@@ -0,0 +1,2 @@
+location            = "westus2"
+resource_group_name = "myTFResourceGroup"
---
+++ .cloud/tf/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+	description = "Azure region for deployed resources."
+	type        = string
+}
+
+variable "resource_group_name" {
+	description = "Name of the Azure resource group."
+	type        = string
+}
